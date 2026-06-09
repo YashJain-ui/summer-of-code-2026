e1463e9d522ae86954c336375bdcf68ccc4eb865
Yash Jain <yj19812006@gmail.com>
2026-06-09 20:22:21 +0530

    Day 1: C++ Basics

---
+++ Data Types.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    char grade = 'A';
+    string phrase = "Very Good";
+    int age; 
+    double gpa = 8.16;
+    bool isMale = true;
+    int num1;
+    int num2;
+
+    cout<< "Enter number 1: ";
+    cin>> num1;
+
+    cout<< "Enter number 2: ";
+    cin>> num2;
+
+    cout<< "Result: "<< num1+num2;
+
+
+
+
+
+    return 0;
+}
---
+++ Variables.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    string cName = "John";
+    int cAge;
+    cAge = 35;
+    cout << "There was " << cName << endl;
+    cout << "Age was " << cAge << endl;
+    cout << "He was " << cAge << "years old"<< endl;
+}
